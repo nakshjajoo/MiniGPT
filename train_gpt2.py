d42109633d1ebc7bb13538ca6c6149906af94e8f
Naksh Jajoo <jajoonaksh01@gmail.com>
2025-04-05 14:27:56 -0500

    implemented weight sharing b/w wte & lm_head

--- train_gpt2.py
+++ train_gpt2.py
@@ -96,6 +96,9 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight # share the weights between the token embedding and the output layer
     
 
     def forward(self, idx, targets=None):
